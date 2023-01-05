bb53ad929df56d660fa18ef458f63d36b36111cd
rohanArhamLabs <rohanlawande@arhamlabs.com>
2023-01-05 12:40:44 +0530

    (feature) customer error functionality

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '8da21d80a51d54702a8ca446e9272ae5cb686ae3',
+    'name' => 'arhamlabs/error-handler',
+  ),
+  'versions' => 
+  array (
+    'arhamlabs/error-handler' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8da21d80a51d54702a8ca446e9272ae5cb686ae3',
+    ),
+  ),
+);
